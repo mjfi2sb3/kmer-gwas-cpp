74277ecae824750abd186c19b1d4300f5d6aba92
mjfi2sb3 <mjfi2sb3@users.noreply.github.com>
2023-10-03 14:32:32 +0300

    1. introduce long options using getopt. 2. introduce count vs absence/presence option. 3. delimiter option: tab vs none

--- src/matrix_merge.cpp
+++ src/matrix_merge.cpp
@@ -1,4 +1,4 @@
-#include "thread_pool.hpp"
+//#include "thread_pool.hpp"
 #include <iostream>
 #include <istream>
 #include <streambuf>
@@ -17,6 +17,8 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <filesystem>
+#include <getopt.h>
+
 
 using namespace std;
 // const size_t NUM_ACC = 100;
@@ -48,7 +50,7 @@ vector<string> get_accessions(string accessions_path)
 	return accessions;
 }
 
-void merge_chunk(const uint file_index, const uint min_occur, string input_path, string accessions_path)
+void merge_chunk(const uint file_index, const uint min_occur, string input_path, string accessions_path, string delimiter, bool show_count, string ouput_dir)
 { 
 
     auto accessions = get_accessions(accessions_path);
@@ -91,9 +93,28 @@ void merge_chunk(const uint file_index, const uint min_occur, string input_path,
     }*/
 
     // size_t num_accessions = accessions.size();
-    std::cout << "Number of accessions: " << NUM_ACC << std::endl;
-    ofstream m_stream("matrix_acc"+to_string(NUM_ACC)+"_"+to_string(min_occur)+"/" + to_string(file_index) + "_matrix.tsv");
-    ofstream ck_stream("matrix_acc"+to_string(NUM_ACC)+"_"+to_string(min_occur)+"/" + to_string(file_index) + "_core.txt");
+    //std::cout << "Number of accessions: " << NUM_ACC << std::endl;
+    /*std::string ouput_dir = "";
+    if (show_count)
+    {
+		ouput_dir = "matrix_acc"+to_string(NUM_ACC)+"_minOcc"+to_string(min_occur)+"_count/";
+    }
+    else
+    {
+    	ouput_dir = "matrix_acc"+to_string(NUM_ACC)+"_minOcc"+to_string(min_occur)+"_pres-abs/";
+    }
+    std:cout << ouput_dir << "\n";
+    if (!filesystem::exists("matrix_acc"+to_string(NUM_ACC)+"_"+to_string(min_occur)))
+		  		filesystem::create_directory("./matrix_acc"+to_string(NUM_ACC)+"_"+to_string(min_occur)+"/");*/
+		  		
+    //std::string ouput_dir = "matrix_acc"+to_string(NUM_ACC)+"_"+to_string(min_occur)+"/";
+    
+    /*ofstream m_stream("matrix_acc"+to_string(NUM_ACC)+"_"+to_string(min_occur)+"/" + to_string(file_index) + "_matrix.tsv");
+    ofstream ck_stream("matrix_acc"+to_string(NUM_ACC)+"_"+to_string(min_occur)+"/" + to_string(file_index) + "_core.txt");*/
+    
+    ofstream m_stream(ouput_dir + to_string(file_index) + "_matrix.tsv");
+    ofstream ck_stream(ouput_dir + to_string(file_index) + "_core.txt");
+    
     for (auto & pair_ : matrix_) {
       // check if a k-mer occurs in all accessions and therefore is flagged as core k-mer
     if (pair_.second[NUM_ACC] == NUM_ACC ){
@@ -105,8 +126,8 @@ void merge_chunk(const uint file_index, const uint min_occur, string input_path,
         auto freqs = pair_.second;
         for (int i = 0; i < NUM_ACC ; i++) {
             auto freq = freqs[i];
-            if (freq > 0) freq = 1;
-            m_stream << freq;
+            if (!show_count && freq > 0) freq = 1;
+            m_stream << delimiter << freq;
         }
             
         m_stream << "\n";    
@@ -118,33 +139,145 @@ void merge_chunk(const uint file_index, const uint min_occur, string input_path,
 
 int main(int argc, char *argv[])
 {
-    if (argc != 5)
+	string input_path, accessions_path;
+	uint file_index;
+	uint min_occur = 6;
+	std::string delimiter = "\t";  // Default value: tab
+	bool show_count = true;  // Default is to show counts
+
+	// Define the long options
+	static struct option long_options[] = {
+		{"input", required_argument, 0, 'i'},
+		{"accessions", required_argument, 0, 'a'},
+		{"index", required_argument, 0, 'f'},
+		{"threshold", required_argument, 0, 't'},
+		{"delimiter", required_argument, 0, 'd'},
+		{"count", required_argument, 0, 'c'},
+		{0, 0, 0, 0}
+	};
+    int option_index = 0;
+    int c;
+    
+    while ((c = getopt_long(argc, argv, "i:a:f:t:d:c:", long_options, &option_index)) != -1) 
+    {
+        switch (c) 
+        {
+            case 'i':
+                input_path = optarg;
+                break;
+            case 'a':
+                accessions_path = optarg;
+                break;
+            case 'f':
+            	if (!all_of(optarg, optarg + strlen(optarg), ::isdigit)) {
+					cerr << "Error: --index requires a valid integer argument." << endl;
+					return -1;
+				}
+				file_index = stoi(optarg);
+				break;
+                /*file_index = stoi(optarg);
+                break;*/
+            case 't':
+                min_occur = stoi(optarg);
+                break;
+            case '?':
+                // getopt_long will print an error message
+                break;
+            case 'd':
+				if (string(optarg) == "tab") 
+				{
+				    delimiter = "\t";
+				} 
+				else if (string(optarg) == "none") 
+				{
+				    delimiter = "";
+				} 
+				else 
+				{
+				    cerr << "Invalid delimiter option. Use 'tab' or 'space'." << endl;
+				    return -1;
+				}
+				break;
+            case 'c':
+				if (string(optarg) == "y") 
+				{
+				    show_count = true;
+				} 
+				else if (string(optarg) == "n") 
+				{
+				    show_count = false;
+				} 
+				else 
+				{
+				    cerr << "Invalid display option. Use 'y' or 'n'." << endl;
+				    return -1;
+				}
+				break;
+            default:
+                abort();
+        }
+    }
+    // Check if all required options are provided
+    if (input_path.empty() || accessions_path.empty()) 
+    /*{
+        cout << "usage: " << argv[0] << " --input <input path> --accessions <accessions path> --index <file index> --threshold <min occurence threshold>\n";
+        return -1;
+    }*/
+    {
+		cout << "\nusage: " << argv[0] << "\n"
+		     << "\t\t--input <input path> \n"
+		     << "\t\t--accessions <accessions path> \n"
+		     << "\t\t--index <file index which corresponds to bin> \n"
+		     << "\t\t--threshold <min/max occurence threshold> (default: " << min_occur << ")\n"
+		     << "\t\t--delimiter <delimiter type: tab|none> (default: " << (delimiter == "\t" ? "tab" : (delimiter == " " ? "space" : "none")) << ")\n"
+		     << "\t\t--count <print matrix as absence/presence or actual k-mer counts; type: y|n> (default: " << (show_count ? "y" : "n") << ")\n\n";
+		return -1;
+	}
+
+    /*if (argc != 5)
     {
         cout << "usage: " << argv[0] << " <input path> <accessions path> <file index> <min occurence threshold (across panel)>\n";
         return -1;
     }
-
+	*/
     try
     {
-      	string input_path = argv[1];
-	string accessions_path = argv[2];
+      	/*string input_path = argv[1];
+		string accessions_path = argv[2];
         uint file_index = stoi(argv[3]);
-	uint min_occur = stoi(argv[4]);
+		uint min_occur = stoi(argv[4]);*/
+		
       	if (input_path[input_path.size()-1] != '/')
-                input_path += '/';
-    auto accessions = get_accessions(accessions_path);
-    size_t NUM_ACC = accessions.size();
-	if (!filesystem::exists("matrix_acc"+to_string(NUM_ACC)+"_"+to_string(min_occur)))
-      		filesystem::create_directory("./matrix_acc"+to_string(NUM_ACC)+"_"+to_string(min_occur)+"/");
+		input_path += '/';
+    	auto accessions = get_accessions(accessions_path);
+   		size_t NUM_ACC = accessions.size();
+   		
+   		std::string ouput_dir = "";
+   		std:string delim = (delimiter == "\t" ? "tab" : (delimiter == " " ? "space" : "none"));
+		if (show_count)
+		{
+			ouput_dir = "matrix_acc"+to_string(NUM_ACC)+"_minOcc"+to_string(min_occur)+"_count_delim-"+delim+"/";
+		}
+		else
+		{
+			ouput_dir = "matrix_acc"+to_string(NUM_ACC)+"_minOcc"+to_string(min_occur)+"_pres-abs_delim-"+delim+"/";
+		}
+   		
+		if (!filesystem::exists(ouput_dir)){
+			filesystem::create_directory(ouput_dir);
+		}
 
         cout << "***************************** " << endl;
         cout << "PROCESSING MATRIX CHUNK: " << file_index << endl;
+        cout << "Number of accessions: " << NUM_ACC << std::endl;
+        cout << "Output Folder: " << ouput_dir << std::endl;
+        
         auto start = chrono::steady_clock::now();
 
-        merge_chunk(file_index, min_occur, input_path, accessions_path);
+        merge_chunk(file_index, min_occur, input_path, accessions_path, delimiter, show_count, ouput_dir);
 
         auto end = chrono::steady_clock::now();
-        cout << "processing index: " << file_index << "took "
+        cout << "processing index: " << file_index << " took "
              << chrono::duration_cast<chrono::seconds>(end - start).count()
              << " sec" << endl;
 
