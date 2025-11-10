c7b74d1b458817803c1ac2eb3ca2b1dcc52c7bb0
mjfi2sb3 <mjfi2sb3@users.noreply.github.com>
2025-11-10 16:21:39 +0300

    - add compilation code on shaheen3

--- src/compile_sh3.sh
+++ src/compile_sh3.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-cmake -DCMAKE_BUILD_TYPE=Release -S/scratch/bougous/kmer-gwas-cpp/src -B/scratch/bougous/kmer-gwas-cpp/build_sh3
-cmake --build /scratch/bougous/kmer-gwas-cpp/build_sh3 --config Release --target all
+#cmake -DCMAKE_BUILD_TYPE=Release -S/scratch/bougous/kmer-gwas-cpp/src -B/scratch/bougous/kmer-gwas-cpp/build_sh3
+#cmake --build /scratch/bougous/kmer-gwas-cpp/build_sh3 --config Release --target all
+
+
+g++ -std=c++17 -O3 -march=native -pthread -o kmer_counter_k51 kmer_count.cpp mmap_io.cpp
+
+g++ -std=c++17 -O3 -march=native -pthread -o matrix_merge matrix_merge.cpp mmap_io.cpp
