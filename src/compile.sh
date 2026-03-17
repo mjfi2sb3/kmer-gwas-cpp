89c01c596724d22d58ad4446119823a4dad63307
mjfi2sb3 <mjfi2sb3@users.noreply.github.com>
2026-03-17 16:19:41 +0300

    Fix short-read bug in kmer_count.cpp
    Skip reads shorter than k=51 to prevent unsigned underflow in the
    inner k-mer extraction loop, which caused std::out_of_range crash.

--- src/compile.sh
+++ src/compile.sh
@@ -9,3 +9,10 @@ g++ -std=c++17 -o matrix_merge_noThread_v2 matrix_merge.cpp -lstdc++fs
 # compile kmc_kmer_binning_ori
 g++ -std=c++17 -O3 -o kmc_kmer_binning_ori kmc_kmer_binning_ori.cpp  /ibex/sw/rl9c/kmc/3.2.1/rl9_conda3/KMC/kmc_api/kmer_api.cpp /ibex/sw/rl9c/kmc/3.2.1/rl9_conda3/KMC/kmc_api/kmc_file.cpp /ibex/sw/rl9c/kmc/3.2.1/rl9_conda3/KMC/kmc_api/mmer.cpp -I. -pthread
 
+
+## for homogeneous systems like shaheen
+g++ -std=c++17 -O3 -march=native -pthread -o kmer_counter_k51 kmer_count_v3.cpp mmap_io.cpp
+## for heterogeneous systems like ibex
+g++ -std=c++17 -O3 -march=x86-64-v2 -pthread -o kmer_counter kmer_count_v3.cpp mmap_io.cpp
+
+g++ -std=c++17 -O3 -march=native -pthread -o matrix_merge matrix_merge.cpp mmap_io.cpp
